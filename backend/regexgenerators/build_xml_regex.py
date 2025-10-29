57bf16cdc86e0c34c6b2b96484c20c9b13e26699
Jonas Schrepfer <jonas@schrepfer-online.de>
2025-10-29 15:05:22 +0100

    Errorhandling XML

--- backend/regexgenerators/build_xml_regex.py
+++ backend/regexgenerators/build_xml_regex.py
@@ -1,15 +1,19 @@
 import re
 from re import Pattern
 from xml.etree import ElementTree
+from typing import Optional
 
-def xml_pattern(string: str) -> Pattern[str]:
-    root = ElementTree.fromstring(string)
-    pattern = re.compile(__build_xml_regex_recursive(element=root, depth=1, max_depth=3))
-    return pattern
+def xml_pattern(string: str) -> Optional[Pattern[str]]:
+    try:
+        root = ElementTree.fromstring(string)
+        pattern = re.compile(__build_xml_regex_recursive(element=root, depth=1, max_depth=3))
+        return pattern
+    except ElementTree.ParseError:
+        print("Fehler beim parsing des XML")
+        return None
 
 def __build_xml_regex_recursive(element: ElementTree.Element, depth: int, max_depth: int) -> str:
     tag = re.escape(element.tag)
-
     open_tag = f"<\\s*{tag}[^>]*>"
     close_tag = f"<\\s*/\\s*{tag}\\s*>"
     if depth > max_depth or len(element) == 0:
